52661e8dc943a721bd5cb02e34f5725577002676
adi <daniel.adrian1234@yahoo.com>
2024-01-25 01:50:52 +0200

    Add project files.

---
+++ Verif_Optional/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Verif_Optional.Data
+{
+    public class DataContext
+    {
+        public class tableContext:DbContext
+        {
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                if (!optionsBuilder.IsConfigured)
+                {
+                    optionsBuilder.UseSqlServer("Data Source=(localdb)\\Local;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+                }
+            }
+        }
+    }
+}
